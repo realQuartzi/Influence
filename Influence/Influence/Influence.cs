d18ceeed8131791c598c5d34f13756347801f609
realQuartzi <34374881+realQuartzi@users.noreply.github.com>
2022-10-06 20:37:51 +0200

    Base Screen Class

--- Influence/Influence/Influence.cs
+++ Influence/Influence/Influence.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Influence
 {
-    internal class Influence
+    public abstract class Influence
     {
+        int width = 512;
+        int height = 512;
+
+        string title;
+
+        public Influence(int width, int height, string title)
+        {
+            this.width = width;
+            this.height = height;
+
+            this.title = title;
+        }
     }
 }
