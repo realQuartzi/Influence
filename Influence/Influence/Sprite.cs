6462eb767d0fd5991a292a7e3a3ae943221a905a
realQuartzi <34374881+realQuartzi@users.noreply.github.com>
2022-10-06 22:45:17 +0200

    base Sprite

---
+++ Influence/Influence/Sprite.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Influence
+{
+    public class Sprite
+    {
+        public Vector2 position;
+        public Vector2 scale;
+
+        public string name = "Sprite";
+        public string tag = "Default";
+
+        public Sprite(int positionX, int positionY, float scaleX, float scaleY)
+        {
+            this.position = new Vector2(positionX, positionY);
+            this.scale = new Vector2(scaleX, scaleY);
+        }
+        public Sprite(Vector2 position, Vector2 scale)
+        {
+            this.position = position;
+            this.scale = scale;
+        }
+        public Sprite(Vector2 position, Vector2 scale, string tag)
+        {
+            this.position = position;
+            this.scale = scale;
+            this.tag = tag;
+        }
+
+    }
+}
