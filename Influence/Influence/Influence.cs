11bd422d3800e8110ca2b5097a3b8491f4683cf5
realQuartzi <34374881+realQuartzi@users.noreply.github.com>
2022-10-06 21:02:29 +0200

    Created Window Class

--- Influence/Influence/Influence.cs
+++ Influence/Influence/Influence.cs
@@ -1,24 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows.Forms;
+using System.Drawing;
 
 namespace Influence
 {
+    class Window : Form
+    {
+        public Window()
+        {
+            this.DoubleBuffered = true;
+        }
+    }
+
     public abstract class Influence
     {
-        int width = 512;
-        int height = 512;
-
+        int width;
+        int height;
         string title;
+
+        Window window = null;
 
         public Influence(int width, int height, string title)
         {
             this.width = width;
             this.height = height;
-
             this.title = title;
+
+            window = new Window();
+            window.Size = new Size(width, height);
+            window.Text = title;
+
+            Application.Run(window);
         }
     }
 }
