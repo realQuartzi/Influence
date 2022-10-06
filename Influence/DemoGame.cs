8b25fe82d970f4f5e072e66333c3744b17bc261f
realQuartzi <34374881+realQuartzi@users.noreply.github.com>
2022-10-06 22:30:36 +0200

    Made small test demo

---
+++ Influence/DemoGame.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Influence
+{
+    class DemoGame : Influence
+    {
+        public DemoGame(): base(512, 512, "Demo Game") { }
+
+        protected override void Initialize()
+        {
+            Console.WriteLine("[Initialized] Called");
+        }
+
+        protected override void Awake()
+        {
+            Console.WriteLine("[Awake] Called");
+        }
+
+        protected override void FixedUpdate()
+        {
+            Console.WriteLine("[Fixed Update] Fixed Time: " + Time.fixedTime + " | Fixed Delta Time: " + Time.fixedDeltaTime);
+        }
+
+        protected override void LateUpdate()
+        {
+
+        }
+
+        protected override void Start()
+        {
+            Console.WriteLine("[Start] Called");
+        }
+
+        int frame = 0;
+        protected override void Update()
+        {
+            frame++;
+            Console.WriteLine("Frame " + frame);
+            Console.WriteLine("[Update] Time: " + Time.time + " | Delta Time: " + Time.deltaTime);
+        }
+    }
+}
