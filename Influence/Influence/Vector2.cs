6e8eb2d7c2c55fd936a0dbf6ffcab9d3788175bd
realQuartzi <34374881+realQuartzi@users.noreply.github.com>
2022-10-06 20:29:24 +0200

    Added Summary

--- Influence/Influence/Vector2.cs
+++ Influence/Influence/Vector2.cs
@@ -17,7 +17,6 @@ namespace Influence
             x = 0;
             y = 0;
         }
-
         public Vector2(float x, float y)
         {
             this.x = x;
@@ -26,11 +25,34 @@ namespace Influence
 
         #region Quick Returns
 
+        /// <summary>
+        /// Returns Vector2 : (0,0)
+        /// </summary>
         public static Vector2 zero => new Vector2(0, 0);
+
+        /// <summary>
+        /// Returns Vector2 : (1,1)
+        /// </summary>
         public static Vector2 one => new Vector2(1, 1);
+
+        /// <summary>
+        /// Returns Vector2 : (0,1)
+        /// </summary>
         public static Vector2 up => new Vector2(0, 1);
+
+        /// <summary>
+        /// Returns Vector2 : (0,-1)
+        /// </summary>
         public static Vector2 down => new Vector2(0, -1);
+
+        /// <summary>
+        /// Returns Vector2 : (-1,0)
+        /// </summary>
         public static Vector2 left => new Vector2(-1, 0);
+
+        /// <summary>
+        /// Returns Vector2 : (1,0)
+        /// </summary>
         public static Vector2 right => new Vector2(1, 0);
 
         #endregion
