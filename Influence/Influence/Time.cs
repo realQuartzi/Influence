00e600c4c9866e3332801370bfbc4a47c55aa3db
realQuartzi <34374881+realQuartzi@users.noreply.github.com>
2022-10-06 22:30:13 +0200

    Added FixedDeltaTime

--- Influence/Influence/Time.cs
+++ Influence/Influence/Time.cs
@@ -11,5 +11,6 @@ namespace Influence
         public static float time { get; set; }
         public static float deltaTime { get; set; }
         public static float fixedTime { get; set; }
+        public static float fixedDeltaTime { get; set; }
     }
 }
