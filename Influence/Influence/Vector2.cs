4b7d4a6b648626bbbaaa35548c335030aa9a4e77
realQuartzi <34374881+realQuartzi@users.noreply.github.com>
2022-10-06 20:27:11 +0200

    Added Quick Getters

--- Influence/Influence/Vector2.cs
+++ Influence/Influence/Vector2.cs
@@ -24,5 +24,16 @@ namespace Influence
             this.y = y;
         }
 
+        #region Quick Returns
+
+        public static Vector2 zero => new Vector2(0, 0);
+        public static Vector2 one => new Vector2(1, 1);
+        public static Vector2 up => new Vector2(0, 1);
+        public static Vector2 down => new Vector2(0, -1);
+        public static Vector2 left => new Vector2(-1, 0);
+        public static Vector2 right => new Vector2(1, 0);
+
+        #endregion
+
     }
 }
