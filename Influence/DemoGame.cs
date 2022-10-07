ee22b647522b9c02f5539a0b5567e194dc4d36b0
realQuartzi <34374881+realQuartzi@users.noreply.github.com>
2022-10-07 17:33:47 +0200

    Sprite Movement Test

--- Influence/DemoGame.cs
+++ Influence/DemoGame.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Drawing;
+using System.Media;
 
 namespace Influence
 {
     class DemoGame : Influence
     {
         public DemoGame(): base(512, 512, "Demo Game") { }
+
+        Sprite player;
 
         protected override void Initialize()
         {
@@ -18,7 +21,7 @@ namespace Influence
             new Shape(new Vector2(32, 32), new Vector3(128, 128, 0), Color.Aqua);
 
             new Sprite("Ensoul", new Vector3(64,64));
-            new Sprite("Ensoul", new Vector3(256, 256), new Vector3(5,5,5));
+            player = new Sprite("Ensoul", new Vector3(32, 256), new Vector3(4,4,4));
         }
 
         protected override void FixedUpdate()
@@ -36,6 +39,8 @@ namespace Influence
 
         protected override void Update()
         {
+            player.transform.Translate(Vector3.right * 100 * Time.deltaTime);
+            Debug.Log("Moving Player");
         }
     }
 }
