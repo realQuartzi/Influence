298a367c6652470bb4a2751292d7168f0a36d0fa
realQuartzi <34374881+realQuartzi@users.noreply.github.com>
2022-10-07 16:12:26 +0200

    Added GameObject class

---
+++ Influence/Influence/Objects/GameObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Influence
+{
+    public class GameObject : Object
+    {
+        public string tag = "Default";
+        public int layer = 0;
+
+        bool active;
+        public bool activeSelf => active;
+        public void SetActive(bool value) => active = value;
+
+        public override bool Equals(object obj)
+        {
+            return obj is GameObject @object
+                && name == @object.name
+                && tag == @object.tag
+                && layer == @object.layer;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
