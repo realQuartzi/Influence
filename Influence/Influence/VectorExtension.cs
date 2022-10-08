158d22848e64f2ce329d49f07caeb537dd3d7fa5
realQuartzi <34374881+realQuartzi@users.noreply.github.com>
2022-10-08 10:53:18 +0200

    New VectorExtensions

---
+++ Influence/Influence/VectorExtension.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Influence
+{
+    static class VectorExtension
+    {
+        public static Vector2Int ToInt(this Vector2 a)
+        {
+            return new Vector2Int((int)a.x, (int)a.y);
+        }
+
+        public static Vector2 ToFloat(this Vector2Int a)
+        {
+            return new Vector2(a.x, a.y);
+        }
+    }
+}
